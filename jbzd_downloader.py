6e56bfde8fc21b407e84616ba04ae9f62277e573
Eroar <erykksc@gmail.com>
2019-11-19 18:20:30 +0100

    Now with -c argument it copies urls from clipboard and downloads them

--- jbzd_downloader.py
+++ jbzd_downloader.py
@@ -1,17 +1,21 @@
 import requests
 import sys
+import os
+from time import gmtime, strftime
 from bs4 import BeautifulSoup
+import pyperclip
 
 """
 arguments:
--i - infinity number of links
--f filename - download memes from text file filename
--c - copy link from clipboard
+-c - copy link\s from clipboard
+? - not supported
+?-i - infinity number of links
+?-f filename - download memes from text file filename
 """
 
 def getTitle(soup):
     titleDiv = soup.find("h3", {"class": "article-title"})
-    title = " ".join(titleDiv.find("a").text.split())
+    title = "-".join(titleDiv.find("a").text.split())
     return title
 
 def getImageFromSoup(soup):
@@ -25,22 +29,51 @@ def getImageFromUrl(url):
     return response.content
 
 def saveToFile(fileName, image):
-    with open("memes//"+ fileName + ".jpg", "wb") as f:
+    with open("memes/" + fileName + ".jpg", "wb") as f:
         f.write(image)
 
-def doEverythingForLink(link):
-    response = requests.get(link)
+def doEverythingForUrl(url):
+    response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
+        
     title = getTitle(soup)
     image = getImageFromSoup(soup)
+    if debug:
+        print("Saving \"" + title + "\" from" + url)
     saveToFile(title, image)
 
-def getMode():
-    if "-i" in sys.argv:
-        return ""
+def getUrlsFromClipboard():
+    urls = pyperclip.paste().replace("\r", "").split("\n")
+    return urls
 
+def filterUrls(urls):
+    """
+    returns a list of jbzd/obr urls
+    """
+    memesUrls = []
+    
+    for url in urls:
+        if url[:24] == "https://jbzd.com.pl/obr/":
+            memesUrls.append(url)
+    return memesUrls
 
+def main():
+    if "-c" in sys.argv:
+        urls = getUrlsFromClipboard()
+        memesUrls = filterUrls(urls)
+
+        if debug:
+            print("Urls:")
+            for memeUrl in memesUrls:
+                print(memeUrl)
+
+        for memeUrl in memesUrls:
+            doEverythingForUrl(memeUrl)
+            
 if __name__ == "__main__":
-    link = sys.argv[1]
-    doEverythingForLink(link)
+    debug = True
+    main()
+    # link = sys.argv[1]
+
+    # doEverythingForUrl(link)
 
